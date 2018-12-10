173679eae7e07bac075fdf2030c8c1c981ad41a9
Martin <martinius96@gmail.com>
2018-12-10 02:40:59 +0100

    Create map4.ino

---
+++ map4.ino
@@ -0,0 +1,51 @@
+#include <Keyboard.h>
+const int tlacidloVpred = 2; 
+int tlacidloStav; 
+int poslednetlacidloStav = HIGH; 
+unsigned long poslednezakmitCas = 0; 
+unsigned long zakmitDelay = 50; 
+int pocitadlo = 0;
+const int tlacidloVlavo = 3;
+const int tlacidloVpravo = 4;
+void setup() {
+  Keyboard.begin();
+  Serial.begin(115200);
+  pinMode(tlacidloVpred, INPUT_PULLUP);
+  pinMode(tlacidloVlavo, INPUT_PULLUP);
+  pinMode(tlacidloVpravo, INPUT_PULLUP);
+}
+
+void loop() {
+  int reading = digitalRead(tlacidloVpred);
+  while(!digitalRead(tlacidloVlavo)){
+  Keyboard.press('a');
+  delay(300);
+  Keyboard.releaseAll();
+    }
+    while(!digitalRead(tlacidloVpravo)){
+    Keyboard.press('d');
+  delay(300);
+  Keyboard.releaseAll();
+    }
+if (reading != poslednetlacidloStav) {
+    poslednezakmitCas = millis();
+  }
+
+  if ((millis() - poslednezakmitCas) > zakmitDelay) {
+    if (reading != tlacidloStav) {
+      tlacidloStav = reading;
+
+      if (tlacidloStav == LOW) {
+        pocitadlo++;
+      }
+    }
+  }
+  poslednetlacidloStav = reading;
+  Serial.println("Hodnota counteru:");
+  Serial.println(pocitadlo);
+if(pocitadlo>=5){
+  Keyboard.write(218);
+  pocitadlo = 0;
+  Serial.println("Pohyb vpred");
+  }
+}
